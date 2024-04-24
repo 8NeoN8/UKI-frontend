38147a30c85603191be89a77f1e747ff22aba8fe
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-04-24 18:34:19 -0400

    Added some global styles, added username Store, and some minor menu changes

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,34 +1,39 @@
 <template>
-  <main class="menu-container">
+  <main class="menu-container flex flex-column justify-between">
 
-    <header class="menu-header">
-      <h1>U KNOW IT</h1>
-      <h3>Another game were friendships are ruined</h3>
-      <h4>Your username: {{username}}</h4>
+    <header class="menu-header test-bg-yellow text-center h-25 flex flex-column justify-center">
+      <div class="fs-3">U KNOW IT</div>
+      <div class="fsh-2">Another game were friendships are ruined</div>
+      <div class="fs-1">Your username: {{username}}</div>
     </header>
 
-    <ul class="menu-main">
+    <ul class="menu-main flex flex-column test-bg-brown h-100">
 
-      <div @click="sendTo('/about')" class="mm-link">Host</div>
-      <div @click="sendTo('/about')" class="mm-link">Join</div>
-      <div @click="sendTo('/about')" class="mm-link">Leaderboard</div>
-      <div @click="sendTo('/about')" class="mm-link">About</div>
-      <div class="mm-link">Change Name</div>
+      <button @click="sendTo('/about')" class="mm-link">Host</button>
+      <button @click="sendTo('/about')" class="mm-link">Join</button>
+      <button @click="sendTo('/about')" class="mm-link">Leaderboard</button>
+      <button @click="sendTo('/about')" class="mm-link">About</button>
+      <button class="mm-link" @click="nameSet = false; tempUsername = ''; username = ''">Change Name</button>
     
     </ul>
 
-    <footer class="menu-footer">
-      <div>discord</div>
-      <div>github</div>
-      <div>patreon</div>
-      <div>twitter</div>
+    <footer class="menu-footer test-bg-purple h-25 overflow-h w-100">
+      <div class="social-links flex justify-around align-center overflow-h">
+        <div class="button-base">discord</div>
+        <div class="button-base">github</div>
+        <div class="button-base">patreon</div>
+        <div class="button-base">twitter</div>
+      </div>
+      <div class="copyright-section">
+        copyright &copy Haskell? I barely know 'er 2024
+      </div>
     </footer>
 
     <div class="username-popup" v-if="!nameSet">
       <div class="username-container">
         <label for="username-input" class="username-input-label">Set your Username</label>
-        <input type="text" class="username-input" name="username-input" v-model="username">
-        <button type="submit" class="username-submit-button" maxLenght=20 @click="checkUsername(username)">Set</button>
+        <input type="text" class="username-input" name="username-input" v-model="tempUsername">
+        <button type="submit" class="username-submit-button" maxLenght=20 @click="checkUsername(tempUsername)">Set</button>
       </div>
     </div>
 
@@ -37,12 +42,9 @@
 </template>
 
 <script lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-
-import { RouterLink } from 'vue-router'
 import { defineComponent } from 'vue'
-import { useCounterStore } from '@/stores/counter'
 
+import { useUserStore } from '@/stores/username'
 
 export default defineComponent({
   // type inference enabled
@@ -52,22 +54,15 @@ export default defineComponent({
   },
   data() {
     return {
-      count: 1,
-      counter: useCounterStore(),
-      username:'',
+      username: '',
+      tempUsername:'',
       nameSet:false,
+      store: useUserStore(),
     }
   },
   mounted() {
-    this.name // type: string | undefined
-    this.msg // type: string
-    this.count // type: number
-
-    
   },
   components:{
-    TheWelcome,
-    RouterLink,
   },
   methods: {
     sendTo(route: string){
@@ -76,6 +71,8 @@ export default defineComponent({
     checkUsername(name: string){
       console.log(name.length);
       name.length <= 16 ? this.nameSet = true : console.log('no go');
+      this.store.user = name
+      this.username = this.store.user
     }
   },
 })
