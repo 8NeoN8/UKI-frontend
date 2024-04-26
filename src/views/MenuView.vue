3836e702cb0d728964242b026906f85fdf7bf38f
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-04-26 18:41:58 -0400

    main menu changes

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,23 +1,36 @@
 <template>
   <main class="menu-container flex flex-column justify-between">
 
-    <header class="menu-header test-bg-yellow text-center h-25 flex flex-column justify-center">
-      <div class="fs-3">U KNOW IT</div>
-      <div class="fsh-2">Another game were friendships are ruined</div>
-      <div class="fs-1">Your username: {{username}}</div>
+    <header class="menu-header test-bg-yellow text-center h-10 text-left flex flex-between">
+
+      <div class="header-left test-bg-lime w-85 flex flex-column justify-center px-1">
+        <div class="game-name-display fsh-3">U KNOW IT</div>
+        <div class="game-subtitle-display fsh-2">Another game where friendships are ruined</div>
+      </div>
+
+      <div class="header-right test-bg-red w-15 center-content">
+        <div class="username-diplay fsh-2">{{username}}</div>
+      </div>
+
     </header>
 
-    <ul class="menu-main flex flex-column test-bg-brown h-100">
+    <div class="menu-main flex justify-between h-80">
+      <ul class="menu-main-options flex flex-column test-bg-brown w-85 justify-start p-2">
+  
+        <button @click="sendTo('/about')" class="mm-link fsh-2 text-left"> <span>Host</span></button>
+        <button @click="sendTo('/about')" class="mm-link fsh-2 text-left">Join</button>
+        <button @click="sendTo('/about')" class="mm-link fsh-2 text-left">Leaderboard</button>
+        <button @click="sendTo('/about')" class="mm-link fsh-2 text-left">About</button>
+        <button class="mm-link fsh-2 text-left" @click="nameSet = false; tempUsername = ''; username = ''">Change Name</button>
+      
+      </ul>
 
-      <button @click="sendTo('/about')" class="mm-link">Host</button>
-      <button @click="sendTo('/about')" class="mm-link">Join</button>
-      <button @click="sendTo('/about')" class="mm-link">Leaderboard</button>
-      <button @click="sendTo('/about')" class="mm-link">About</button>
-      <button class="mm-link" @click="nameSet = false; tempUsername = ''; username = ''">Change Name</button>
-    
-    </ul>
+      <aside class="menu-main-news center-content w-15 test-bg-cyan">
+        "news" o cualquier vaina que queramos poner aqui
+      </aside>
+    </div>
 
-    <footer class="menu-footer test-bg-purple h-25 overflow-h w-100">
+    <footer class="menu-footer test-bg-purple h-10 overflow-h w-100">
       <div class="social-links flex justify-around align-center overflow-h">
         <div class="button-base">discord</div>
         <div class="button-base">github</div>
@@ -25,11 +38,11 @@
         <div class="button-base">twitter</div>
       </div>
       <div class="copyright-section">
-        copyright &copy Haskell? I barely know 'er 2024
+        copyright &copy; Haskell? I barely know 'er 2024
       </div>
     </footer>
 
-    <div class="username-popup" v-if="!nameSet">
+    <div class="username-popup test-bg-purple" v-if="!nameSet">
       <div class="username-container">
         <label for="username-input" class="username-input-label">Set your Username</label>
         <input type="text" class="username-input" name="username-input" v-model="tempUsername">
@@ -60,6 +73,12 @@ export default defineComponent({
       store: useUserStore(),
     }
   },
+  created() {
+    if(this.store.user != ''){
+      this.username = this.store.user
+      this.nameSet = true
+    }
+  },
   mounted() {
   },
   components:{
@@ -69,7 +88,6 @@ export default defineComponent({
       this.$router.push(route)
     },
     checkUsername(name: string){
-      console.log(name.length);
       name.length <= 16 ? this.nameSet = true : console.log('no go');
       this.store.user = name
       this.username = this.store.user
